db04290d5f6d00ed2c66bf270ecedf3e494f7811
Hunter <huntersfred@gmail.com>
2023-04-01 14:55:01 -0400

    feat(Project): Add UI Asset
    In this commit, we have further improved the user interface by integrating a new UI asset to provide a richer set of UI elements for input fields, sliders, scroll bars, and more.
    
    Add a UI asset for advanced UI fields:
    We have added a new UI asset to the Unity project that includes a variety of UI elements such as input boxes, sliders, scroll bars, and other interactive components. This asset enhances the overall user experience by providing a more comprehensive and polished set of UI elements for players to interact with.
    
    Update existing UI to incorporate new UI elements:
    We have updated the existing UI, such as the shop interface and other game menus, to incorporate the new UI elements from the additional asset. This ensures a consistent and visually appealing user experience across all parts of the game.
    
    Changes Made:
    - Added UI Elements asset

---
+++ Project-Kratos/Assets/Plugins/DarkGreenUI/Scripts/Window.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Window : MonoBehaviour {
+
+    [Header("Buttons")]
+    public bool showCloseButton = true;
+    public bool showOptionsButton = true;
+
+    public GameObject closeButton;
+    public GameObject optionsButton;
+    public GameObject settingsMenu;
+
+    private Animator animator;
+
+    private void Start() {
+        animator = settingsMenu.GetComponent<Animator>();
+    }
+
+    private void OnValidate() {
+        if (closeButton != null) closeButton.SetActive(showCloseButton);
+        if (optionsButton != null) optionsButton.SetActive(showOptionsButton);
+    }
+
+    public void OnSettingsButtonClick() {
+        bool isOpen = animator.GetBool("isOpen");
+        animator.SetBool("isOpen", !isOpen);
+    }
+
+    public void OnCloseButtonClick() {
+        gameObject.SetActive(false);
+    }
+}

d021a20fc767eacf01d296c871fb97a947a9f531
Hunter <huntersfred@gmail.com>
2023-05-10 20:46:06 -0400

    feat: Shop Changes

--- Project-Kratos/Assets/_Game/UI/Shop/Items/AbilityItem.cs
+++ Project-Kratos/Assets/_Game/UI/Shop/Items/AbilityItem.cs
@@ -6,10 +6,13 @@ namespace ProjectKratos.Shop
     public class AbilityItem : ShopItem
     {
         [SerializeField] private PlayerAbility _ability;
-
+        [SerializeField] private GameObject _shopMenu;
+        
         public override void BuyItem()
         {
             _variables.SetNewAbility(_ability);
+            
+            _shopMenu.SetActive(false);
         }
     }
 }
--- Project-Kratos/Assets/_Scripts/Events/ToggleEnabled.cs
+++ Project-Kratos/Assets/_Scripts/Events/ToggleEnabled.cs
@@ -1,4 +1,6 @@
+using System;
 using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 
 namespace ProjectKratos.Events
@@ -6,8 +8,21 @@ namespace ProjectKratos.Events
     public class ToggleEnabled : MonoBehaviour
     {
 
+        private TMP_Text _text;
+
+        private void Awake()
+        {
+            _text = GetComponentInChildren<TMP_Text>();
+        }
+
         [Button]
-        public void ToggleGameobject(GameObject toggle) => toggle.SetActive(!toggle.activeSelf);
+        public void ToggleGameobject(GameObject toggle)
+        {
+            toggle.SetActive(!toggle.activeSelf);
+
+            _text.text = toggle.activeSelf ? "Exit" : "Shop";
+        }
+
 
     }
 }
